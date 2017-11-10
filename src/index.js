3e259dcf11b9bae8b192347e50a63085cf546b0c
Nick Clarity <nickc@magenic.com>
2017-11-10 10:25:03 -0600

    added redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+console.log('Test');
+
+const userInfoReducer = (state = {}, action) => {
+    
+    switch(action.type){
+        case "UPDATE_USER_NAME":
+            return {... state, name: action.payload }
+        case "UPDATE_USER_AGE":
+            return {... state, age: action.payload }        
+        default:
+            return state
+    }
+}
+
+const preferredGreeting = (state = {}, action) => {
+    return state;
+}
+
+const reducers = combineReducers({
+    userInfoReducer: userInfoReducer,
+    preferredGreeting: preferredGreeting
+})
+
+const applicationStore = createStore(reducers, {
+    userInfoReducer: {
+        name: "Nick",
+        age: 33,
+        email: "nickc@magenic.com"
+    },
+    preferredGreeting: "hello"
+});
+
+console.log(applicationStore.getState());
+
+applicationStore.subscribe(() => {
+    console.log("Store has been updated", applicationStore.getState());
+})
+
+
+applicationStore.dispatch({type: "UPDATE_USER_NAME", payload: "Morty"});
+applicationStore.dispatch({type: "UPDATE_USER_AGE", payload: "14"});
